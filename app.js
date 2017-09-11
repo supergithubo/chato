d8e42a9ca707036f8f66367f194c20136aafaa07
supergithubo <winston.los.santos@gmail.com>
2017-09-11 23:07:40 +0800

    Setup db

--- app.js
+++ app.js
@@ -1,21 +1,52 @@
+
+// Base Setup
+// =======
+
 var express = require("express");
 var bodyParser = require("body-parser");
-
-var webhookRouter = require("./routes/webhook.route");
+var mongoose = require('mongoose');
 
 var app = express();
+
+var webhookRouter = require("./routes/webhook.route");
+var recipeRouter = require("./routes/recipe.route");
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-var server = app.listen((process.env.PORT || 5000));
+// Routes
+// =============================================================================
 
-app.use('/v1', [webhookRouter]);
+app.use('/v1', [webhookRouter, recipeRouter]);
 app.use(function(err, req, res, next) {
     console.log(err);
     if(err.name == 'ValidationError' || err.message == 'validation error') {
         return res.status(422).send(err);
     }
     return res.status(500).send(err);
+});
+
+// Server Start
+// =============================================================================
+
+var port = process.env.PORT || 5000;
+var server = app.listen(port);
+
+console.log('Listening on port ' + port);
+
+// Database Start
+// =============================================================================
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DB_URI, {
+    useMongoClient: true
+});
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Connection error: '));
+db.once('open', function() {
+    console.log('Connected to database on ' + config.db.uri);
 });
 
 module.exports = server;
